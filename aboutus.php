892c0a335c58f6c3b2a93220098c682c0c88f5b5
git-shru <mca.1904@unigoa.ac.in>
2020-11-25 14:56:06 +0530

    about us page

---
+++ aboutus.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Bank And Donor System</title>
+</head>
+<body>
+<button  onclick="document.location='index.php'" >HOME</button>
+<center>
+<img src="4.jpg"  width="300" height="300">
+<h3>The main goal of the Blood Bank and Donor Management System project is to monitor Blood Bank data, Blood stock, Donor List.</h3>
+<h3>It manages all the Blood Bank, Donor, Blood stock data. The project is entirely administrative and therefore access is guaranteed only to the administrator.</h3>
+<h3>The project's aim is to develop an application system to minimize the manual work for Blood Bank, Donor, Blood Group management.</h3>
+<h3>It monitors all of the Blood Group information, Blood supply and Donor list.</h3></pre></center><br><hr>
+<address>
+LIFELINE PRIVATE LTD
+<br>
+1111111111
+<br>
+BICHOLM GOA
+<br>
+403504
+</address>
+</body>
+</html>
