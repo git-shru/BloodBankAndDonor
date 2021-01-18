9c12c716470c6e95d8a1595acb1b0b483dad6372
git-shru <70577789+git-shru@users.noreply.github.com>
2021-01-18 19:03:38 +0530

    Delete sample.php

--- sample.php
+++
@@ -1,9 +0,0 @@
- <form action ="home.php" method = "post">
-  
-          <input name="search" type="text" size="30" placeholder="Belfast"/>
-
-          <input type="submit" value="Search"/>
-
-          </form> 
-
-          <?php print ("$output");?>
